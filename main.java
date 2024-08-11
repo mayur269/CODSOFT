2f3550b99935fd3745aa2fa8ca57f14a5e5e6c44
Mayur Dongare <144617577+mayur269@users.noreply.github.com>
2024-08-11 22:09:54 +0530

    Delete main.java

--- main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        BankAccount myAccount = new BankAccount(5000); // Initial balance of ₹5000
-        ATM atm = new ATM(myAccount);
-        atm.showATMInterface();
-    }
-}
